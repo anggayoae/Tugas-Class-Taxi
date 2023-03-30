eda5d17dfe87472f616a049f7163a582bb4a7d84
anggayoae <127918323+anggayoae@users.noreply.github.com>
2023-03-30 20:58:05 +0700

    Tugas-Class-Taxi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanTaxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.Drivername = "Angga";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanTaxi
+{
+    internal class Taxi
+    {
+        public string Drivername { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Driver : {0}", Drivername);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine ("Number of passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+
+            Console.WriteLine ("{0} sedang menjemput penumpang", Drivername);
+
+        }
+        public void DropOffPassenger()
+        {
+
+            Console.WriteLine ("{0} sudah selesai mengantar penumpang", Drivername);
+
+        }
+    }
+}
